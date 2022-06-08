e070fa364ede36fdeee02e77ad08215357ba7122
Ole Stender <o.m.stender@gmail.com>
2022-06-08 10:03:25 +0200

    Impressum 1

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,8 +10,17 @@ const Footer = () => {
                 </div>
                 <div>
                     <h3>Impressum</h3>
-                    <p>Gewiss e.V.</p>
-                    <p>Musterstraße 123 30171 Hannover</p>
+                    <p>Gewiss - Generationen wohnen im Schloss e.V., Hildesheim</p>
+                    <p>Bischhof-Gerhard-Str. 11, 31139 Hildesheim</p>
+                    <h4>Gemeinschaftlich Vertretungsberechtigt:</h4>
+                    <p>Julian Franke (1. Vorsitzender)<br/>
+                    E-Mail: Franken.julian@web.de<br/>
+                    Tel.: 0177 9412704</p>
+                    <p>Anna Knetsch (2. Vorsitzende)</p>
+                    <p>Vera Lemke (Schriftführerin)</p>
+                    <p>Dorothee Kochskämper (Kassenwärtin)</p>
+                    <p><b>Registergericht:</b> Amtsgericht Hildesheim</p>
+                    <p><b>Registernummer:</b> 201512</p>
                 </div>
             </div>
         </footer>
--- components/Leitsatz.tsx
+++ components/Leitsatz.tsx
@@ -7,8 +7,8 @@ const Leitsatz = () => {
     return (
         <div className={styles.leitsatz}>
             <LeitsatzItem highlights={marks} direction="left" text="Gewiss ist eine Vielfalt liebende Gemeinschaft, die einen Raum zur Selbstverwirklichung schaffen will." image="/faust.svg"/>
-            <LeitsatzItem highlights={marks} direction="right" text="Wir wollen Strukturen schaffen, die Sicherheit stiftet und unterschiedliche Lebensumstände berücksichtigen." image="/struktur.svg"/>
-            <LeitsatzItem highlights={marks} direction="left" text="Die Strukturen soll auch gegenüber sich ändernden Anforderungen durch gleichberechtigte Mitgestaltung offen bleiben. " image="/waage.svg"/>
+            <LeitsatzItem highlights={marks} direction="right" text="Wir möchten Strukturen schaffen, die Sicherheit stiften und unterschiedliche Lebensumstände berücksichtigen." image="/struktur.svg"/>
+            <LeitsatzItem highlights={marks} direction="left" text="Die Strukturen soll auch gegenüber sich ändernden Anforderungen offen bleiben. Das erreichen wir durch gleichberechtigte Mitgestaltung. " image="/waage.svg"/>
             <LeitsatzItem highlights={marks} direction="right" text="Der achtsame Umgang mit unseren Mitmenschen, unserer Umwelt und uns selbst ist uns sehr wichtig." image="/blume.svg"/>
         </div>
     )
