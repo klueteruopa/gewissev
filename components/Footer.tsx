80e0bb47a61732b56f72f5162ec5b73dbb0c23b8
JFgewiss <113657887+JFgewiss@users.noreply.github.com>
2023-06-04 12:33:34 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,12 +13,9 @@ const Footer = () => {
                     <p>Gewiss - Generationen wohnen im Schloss e.V., Hildesheim</p>
                     <p>Bischhof-Gerhard-Str. 11, 31139 Hildesheim</p>
                     <h4>Gemeinschaftlich Vertretungsberechtigt:</h4>
-                    <p>Julian Franken (1. Vorsitzender)<br/>
-                    E-Mail: julian@gewiss-ev.eu<br/>
-                    Tel.: 0177 9412704</p>
-                    <p>Anna Knetsch (2. Vorsitzende)</p>
-                    <p>Vera Lemke (Schriftführerin)</p>
-                    <p>Dorothee Kochskämper (Kassenwärtin)</p>
+                    <p>Vera Lemke (1. Vorsitzende)<br/>
+                    E-Mail: vera@gewiss-ev.eu<br/>
+                    Tel.: 01573 2272892</p>
                     <p><b>Registergericht:</b> Amtsgericht Hildesheim</p>
                     <p><b>Registernummer:</b> 201512</p>
                 </div>
